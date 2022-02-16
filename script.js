b98a97fe06d72ab53b2652464d46ec3a8a974afc
TangGiaoSu <19522181@gm.uit.edu.vn>
2022-02-16 22:27:38 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var mybutton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function Search(){
+    location.reload();
+}
+
+function GotoTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
